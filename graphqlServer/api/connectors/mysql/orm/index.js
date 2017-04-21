d825e40e3c6b4f157b4d74149c1ab1fbff9fd4ad
miwo100 <mwolff@cognovo.de>
2017-04-21 10:37:40 +0000

    removed js and js.map files from source control

--- graphqlServer/api/connectors/mysql/orm/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var awtModelDefiner_1 = require("./awtModelDefiner");
-exports.awtModelDefiner = awtModelDefiner_1.awtModelDefiner;
-//# sourceMappingURL=index.js.map
--- graphqlServer/api/schemas/githubSchema.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var githubSchema = "\nenum IssueState{\n    OPEN\n    CLOSED\n}\ntype Issue {\n    id: ID\n    author: String\n    body: String\n    createdAt: String\n    number: Int\n    state: IssueState\n    title: String\n}\n\ntype Repository {\n    id: ID\n    description: String\n    isPrivate: Boolean\n    issues(first: Int, after: String, last: Int, before: String,issueStates: [IssueState]!): [Issue]\n    name: String\n}\n";
-exports.githubSchema = githubSchema;
-exports.resolvers = {};
-//# sourceMappingURL=githubSchema.js.map
